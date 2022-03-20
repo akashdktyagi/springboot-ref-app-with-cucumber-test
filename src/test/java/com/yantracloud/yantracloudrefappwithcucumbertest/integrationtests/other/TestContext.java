ff0e5d76416b8d888bbb6798fc160b86b859a4cb
akashtyagi <akashdktyagi@gmail.com>
2022-03-20 20:17:43 +0800

    Added an new Class call as TestContext to to be injected and act as a wrapper on top of MockMvc things like body, url etc.

--- src/test/java/com/yantracloud/yantracloudrefappwithcucumbertest/integrationtests/other/TestContext.java
+++ src/test/java/com/yantracloud/yantracloudrefappwithcucumbertest/integrationtests/other/TestContext.java
@@ -1,5 +1,6 @@
 package com.yantracloud.yantracloudrefappwithcucumbertest.integrationtests.other;
 
+import io.cucumber.spring.ScenarioScope;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.ResultActions;
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+@ScenarioScope
 public class TestContext {
     ResultActions resultActions;
     String body;
