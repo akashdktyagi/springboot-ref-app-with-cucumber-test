3a41260f988cdb62a6a3481aa220531dea9e2bc9
akashtyagi <akashdktyagi@gmail.com>
2022-03-20 20:08:44 +0800

    added two config yml files, one for production and other for test config. Usage of Spring Profile. Added cucumbertest config segment in 'test' spring profile

---
+++ src/test/java/com/yantracloud/yantracloudrefappwithcucumbertest/integrationtests/other/TestContext.java
@@ -0,0 +1,21 @@
+package com.yantracloud.yantracloudrefappwithcucumbertest.integrationtests.other;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.ResultActions;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class TestContext {
+    ResultActions resultActions;
+    String body;
+    String url;
+    String access_token;
+    HttpHeaders httpHeaders = new HttpHeaders();
+}
