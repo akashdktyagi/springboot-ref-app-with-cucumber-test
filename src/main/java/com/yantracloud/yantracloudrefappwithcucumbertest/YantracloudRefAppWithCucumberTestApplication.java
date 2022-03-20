08633147f20c666b0b3ab43e8a82d4295384f624
akashtyagi <akashdktyagi@gmail.com>
2022-03-20 20:36:42 +0800

    Comment in main app

--- src/main/java/com/yantracloud/yantracloudrefappwithcucumbertest/YantracloudRefAppWithCucumberTestApplication.java
+++ src/main/java/com/yantracloud/yantracloudrefappwithcucumbertest/YantracloudRefAppWithCucumberTestApplication.java
@@ -12,10 +12,9 @@ public class YantracloudRefAppWithCucumberTestApplication {
 
 }
 
-/*
+/* Command to get the Token
 curl --request POST \
   --url https://dev-j8ajv706.us.auth0.com/oauth/token \
   --header 'content-type: application/json' \
   --data '{"client_id":"L8XRJjXgjJqsrUCcELtVb5m46KqWgLlf","client_secret":"5PglHF_jc5v-81fzlJcOTM2YZ2iABNYAuekfO_WR3W4IHKN0-7yxPqcbdHO0LI65","audience":"http://localhost:9096/api","grant_type":"client_credentials"}'
-
  */
